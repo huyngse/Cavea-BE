be545dbfddd145fc55b512851d820be2fd7a1fee
Leficios1 <116586512+Leficios1@users.noreply.github.com>
2023-10-20 15:08:49 +0700

    SendEmail: Send email and verify email (#7)
    * SendEmail: Send email and verify email
    
    * SendEmail: restPassWord
    
    * SendEmail: fix send email
    
    ---------
    
    Co-authored-by: Cuongse173196 <cuongpdse173196@fpt.edu.vn>

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.DTO.LoginDTO;
+import com.example.demo.DTO.TokenDTO;
+import com.example.demo.DTO.UserNameDTO;
 import com.example.demo.DTO.registerDTO;
 import com.example.demo.Entities.Account;
 import com.example.demo.Service.AccountService;
@@ -32,23 +34,39 @@ public class AccountController {
 		return null;
 	}
 
+	@PostMapping("/getAccount")
+	public Account getAccountByUsername(@RequestBody UserNameDTO DTO) {
+		String username = DTO.getUsername();
+		Account getAccount = accservice.getAccountByUserName(username);
+		if (getAccount == null) {
+			throw new RuntimeException("Not found");
+		}
+		return getAccount;
+
+	}
+
 	@PostMapping("/checklogin")
 	public Account checkLogin(@RequestBody LoginDTO loginDTO) {
 		String username = loginDTO.getUsername();
 		String password = loginDTO.getPassword();
 		Account checklogin = accservice.checkLogin(username, password);
-		if (checklogin != null) {
-			return checklogin;
+		if (checklogin == null) {
+			throw new RuntimeException("Not found account");
 		}
-		return null;
+		return checklogin;
 	}
 
 	@PostMapping("/register")
 	public Account register(@RequestBody registerDTO registerDTO) {
 		Account account = accservice.register(registerDTO);
 		if (account == null) {
-			return null;
+			throw new RuntimeException("Error at Account Controller");
 		}
 		return account;
+	}
+
+	@PostMapping("/registerVerify")
+	public void registerVerify(@RequestBody TokenDTO DTO) {
+		accservice.setEnable(DTO.getToken());
 	}
 }
